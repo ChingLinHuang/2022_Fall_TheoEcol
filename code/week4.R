3da697fc692f333f76ad04463137a88cbae3d2cd
Katasuke <clhuang12732@gmail.com>
2022-10-03 17:57:51 +0800

    20221003

--- code/week4.R
+++ code/week4.R
@@ -28,9 +28,9 @@ points(N ~ times, data = pop_size)
 ###### Part 2: Bifurcation curve
 ### (1) data setting
 # intrinsic growth rate sequence
-r_seq <- seq(from = 2.82, to = 2.83, by = 0.001)
+r_seq <- seq(from = 1.8, to = 3, by = 0.001)
 # number of sampling
-N_rep <- 5000
+N_rep <- 100
 
 # data
 dat_plot <- data.frame(r = rep(r_seq, each = N_rep), N = 0)
@@ -43,7 +43,7 @@ for (lo in 1:length(r_seq)){
   r <- r_seq[lo]
   K <- 500
   N0 <- 10
-  time <- 50000
+  time <- 1000
 
   pop_size <- data.frame(times = 1:time)
   pop_size$N[1] <- N0
@@ -56,4 +56,4 @@ for (lo in 1:length(r_seq)){
   dat_plot$N[(1 + (lo - 1)*N_rep):(lo*N_rep)] <- pop_size$N[(nrow(pop_size) - N_rep + 1):nrow(pop_size)]
 }
 
-plot(N ~ r, data = dat_plot, cex = 0.1, pch = 20)
+plot(N ~ r, data = dat_plot, cex = 0.7, pch = 20)
--- code/week5.R
+++ code/week5.R
@@ -1,87 +1,91 @@
 # week 5
 
-library(tidyverse)
-
-#### Part 1
 ### Leslie matrix and initial age classes
-leslie_mtrx <- matrix(data = c(0, 1, 5,
+leslie <- matrix(data = c(0, 1, 5,
                                0.5, 0, 0,
                                0, 0.3, 0),
-                      nrow = 3, 
+                      nrow = 3,
                       ncol = 3,
                       byrow = T)
 
-initial_age <- c(10, 0, 0)
+N0 <- c(10, 0, 0)
 
 ### for loop and matrix algebra
 time <- 50
-pop_size <- data.frame(Age1 = numeric(time+1),
-                       Age2 = numeric(time+1),
-                       Age3 = numeric(time+1))
-pop_size[1, ] <- initial_age
+pop_size <- data.frame(Age1 = 0,
+                       Age2 = 0,
+                       Age3 = 0)
+pop_size[1, ] <- N0
 
-for (i in 1:time) {
-  pop_size[i+1, ] <- leslie_mtrx %*% as.matrix(t(pop_size[i, ]))
+for (i in 2:time) {
+  pop_size[i, ] <- leslie %*% t(pop_size[i-1, ])
 }
 
-pop_size <- pop_size %>% 
-  round() %>%
-  mutate(Total_N = rowSums(.), 
-         Time = 0:time) %>%
-  relocate(Time)
+# Total abundance
+pop_size$N <- rowSums(pop_size)
 
-head(round(pop_size)) 
+head(pop_size)
 
-### Asymptotic growth rate and stable age distribution 
-asymptotic_growth <- round(pop_size[time+1, 5]/pop_size[time, 5], 3)
+plot(c(1,time), c(0,265), type = "n", ann = F)
+lines(1:time , pop_size$Age1, col = "red")
+lines(1:time , pop_size$Age2, col = "blue")
+lines(1:time , pop_size$Age3, col = "green")
+legend("topleft",
+       legend = c("Age1", "Age2", "Age3"),
+       col = c("red", "blue", "green"),
+       lty = 1)
+
+
+### Asymptotic growth rate and stable age distribution
+
+asymptotic_growth <- pop_size$N[time]/pop_size$N[time-1]
 asymptotic_growth
 
-age_distribution <- round(pop_size[time+1, 2:4]/sum(pop_size[time+1, 2:4]), 3)
+age_distribution <- pop_size[time, 1:3]/sum(pop_size[time, 1:3])
 age_distribution
 
-### Eigenanalysis of the Leslie matrix
-eigen_out <- eigen(leslie_mtrx)
-as.numeric(eigen_out$values[1]) %>% round(., 3)  # dominant eigenvalue
-as.numeric(eigen_out$vectors[, 1]/sum(eigen_out$vectors[, 1])) %>% 
-  round(., 3)  # stable age distribution
+### Eigen-analysis of the Leslie matrix
+EIGEN <- eigen(leslie)
+EIGEN
+as.numeric(EIGEN$values[1]) # dominant eigenvalue
+as.numeric(EIGEN$vectors[, 1] / sum(EIGEN$vectors[, 1])) # corresponding eigenvector
 
-#### Part 2 - Visualizing population dynamics and age distribution
-### Population sizes for each age class
-pop_size %>%
-  pivot_longer(cols = -Time, names_to = "Age_class", values_to = "N") %>%
-  ggplot(aes(x = Time, y = N, color = Age_class)) + 
-  geom_point() + 
-  geom_line() + 
-  labs(x = "time", y = expression(italic(N))) +
-  theme_classic(base_size = 12) +
-  scale_x_continuous(limits = c(0, time*1.05), expand = c(0, 0)) +
-  scale_y_log10(limits = c(1, max(pop_size$Total_N)*1.05), expand = c(0, 0)) + 
-  scale_color_manual(values = c("#E41A1C", "#377EB8", "#4DAF4A", "black"),
-                     name = NULL,
-                     label = c("Age1", "Age2", "Age3", "Total"))
 
-### Stable age distribution
-library(gganimate)
+##### Part 2
+library(ggplot2)
+set.seed(1234)
+MAT <- matrix(rnorm(25), ncol = 5, nrow = 5)
+abs(eigen(MAT)$values) # check only one dominant eigenvalue
 
-age_animate <- pop_size %>% 
-  mutate(across(Age1:Age3, function(x){x/Total_N})) %>%
-  select(Time, Age1:Age3) %>%
-  pivot_longer(Age1:Age3, names_to = "Age", values_to = "Proportion") %>%
-  ggplot(aes(x = Age, y = Proportion, fill = Age)) + 
-  geom_bar(stat = "identity", show.legend = F) +
-  labs(x = "") +
-  scale_y_continuous(limits = c(0, 1), breaks = seq(0, 1, 0.1), expand = c(0, 0)) +
-  scale_fill_brewer(palette = "Set1") + 
-  theme_classic(base_size = 12) + 
-  transition_manual(Time) + 
-  ggtitle("Time {frame}") + 
-  theme(title = element_text(size = 15))
+v <- rnorm(5)
+time <- 15
 
-anim_save("age_distribution.gif", age_animate, nframes = time + 1, fps = 4, width = 5, height = 4, units = "in", res = 300)
+dat_v <- data.frame(matrix(ncol = 5, nrow = time))
+dat_v[1, ] <- v
+for(i in 2:time){
+  dat_v[i, ] <- MAT %*% t(dat_v[i-1, ])
+}
 
+# Remake data for gganimate
+dat <- data.frame(X1 = 0, X2 = 0, Time = 1)
+for(i in 1:time){
+  dat <- rbind(dat, data.frame(dat_v[i,1:2] / sqrt(sum(dat_v[i,1:2]^2)) * i, Time = i))
+  dat <- rbind(dat, c(0,0, i+1))
+}
+dat <- dat[-nrow(dat), ]
+
+
+ggplot(dat, aes(X1, X2, color = Time)) +
+  geom_path(arrow = arrow(length = unit(0.55, "cm")))
 
 ##### Part 3
-leslie <- matrix(c(0,0,0,0,0,322.38,0.966,0,0,0,0,0,0.013,0.01,0.125,0,0,3.448,0.007,0,0.125,0.238,0,30.17,0.008,0,0.023,0.245,0.167,0.862,0,0,0,0,0.75,0), ncol = 6, byrow = T)
+leslie <- matrix(c(0    ,0   ,0    ,0    ,0    ,322.38,
+                   0.966,0   ,0    ,0    ,0    ,0,
+                   0.013,0.01,0.125,0    ,0    ,3.448,
+                   0.007,0   ,0.125,0.238,0    ,30.17,
+                   0.008,0   ,0.023,0.245,0.167,0.862,
+                   0    ,0   ,0    ,0    ,0.75 ,0)
+                 , ncol = 6, byrow = T)
 
 eigen(leslie)$values[1]
 
